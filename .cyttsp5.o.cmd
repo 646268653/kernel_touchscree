71b621e50d3335053c55e398b65f36cb7de3fce1
646268653 <646268653@qq.com>
2017-07-11 18:56:42 +0800

    add the kernel touch dirver files

---
+++ .cyttsp5.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/input/touchscreen/cyttsp5.o := arm-linux-ld -EL    -r -o drivers/input/touchscreen/cyttsp5.o drivers/input/touchscreen/cyttsp5_core.o drivers/input/touchscreen/cyttsp5_mt_common.o drivers/input/touchscreen/cyttsp5_mta.o 
